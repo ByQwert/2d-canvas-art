c198ae84a20e8ba59b53857d7014a31cfb0746e1
Anton <byqwerton@gmail.com>
2017-01-09 22:31:36 +0200

    Edit canvas height

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@ function getRandomInt(min, max) {
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
 canvas.width = 600;
-canvas.height = 600;
+canvas.height = 300;
 document.body.insertBefore(canvas,document.getElementById("credits"));
 
 // Events for canvas
